aa00a9f78632ae1df3df8b99b97d466d414f7306
Decio Neto <deciom.neto10@gmail.com>
2022-02-13 00:34:46 -0300

    feat: javascript animation

--- src/App.vue
+++ src/App.vue
@@ -6,80 +6,120 @@
     <h2 v-else key="secondary">Hello, Vue</h2>
   </transition> -->
 
-  <transition name="zoom" type="animation" appear>
+  <!-- <transition name="zoom" type="animation" appear>
     <h2 v-show="flag">Hello</h2>
+  </transition> -->
+
+  <transition appear :css="false"
+    @before-enter="beforeEnter"
+    @enter="enter"
+    @after-enter="afterEnter"
+    @before-leave="beforeLeave"
+    @leave="leave"
+    @after-leave="afterLeave"
+  >
+    <h2 v-show="flag">Hey! JS!</h2>
   </transition>
 </template>
-
 <script>
 
 export default {
   name: 'App',
-  methods: {
-    changeFlag() {
-      this.flag = !this.flag;
-    }
-  },
   data() {
     return {
       flag: true,
+    }
+  },
+  methods: {
+    changeFlag() {
+      this.flag = !this.flag;
+    },
+    beforeEnter(el) {
+      console.log('before-enter event fired', el)
+    },
+    enter(el, done) {
+      console.log('enter event fired', el)
+      const animation = el.animate([{ transform: 'scale3d(0,0,0)' }, {}], {
+        duration: 1000,
+      })
+      animation.onfinish = () => {
+        done()
+      }
+    },
+    afterEnter(el) {
+      console.log('after-enter event fired', el)
+    },
+    beforeLeave(el) {
+      console.log('before-leave event fired', el)
+    },
+    leave(el, done) {
+      console.log('leave event fired', el)
+      const animation = el.animate([{}, { transform: 'scale3d(0,0,0)' }], {
+        duration: 1000,
+      })
+      animation.onfinish = () => {
+        done()
+      }
+    },
+    afterLeave(el) {
+      console.log('after-leave event fired', el)
     }
   }
 };
 </script>
 
 <style>
-h2 {
-  width: 400px;
-  padding: 20px;
-  margin: 20px;
-}
-.fade-enter-from {
-  opacity: 0;
-}
-
-.fade-enter-active {
-  transition: all 0.25s linear;
-}
-
-.fade-leave-to {
-  transition: all 0.25s linear;
-  opacity: 0;
-}
-
-.zoom-enter-active {
-  animation: zoom-in 1s linear forwards;
-  transition: all 1s linear;
-}
-
-.zoom-leave-active {
-  animation: zoom-out 1s linear forwards;
-  transition: all 1s linear;
-}
-
-.zoom-enter-from {
-  opacity: 0;
-}
-
-.zoom-leave-to {
-  opacity: 0;
-}
-
-@keyframes zoom-in {
-  from {
-    transform: scale(0, 0);
+  h2 {
+    width: 400px;
+    padding: 20px;
+    margin: 20px;
   }
-  to {
-    transform: scale(1, 1);
+  .fade-enter-from {
+    opacity: 0;
   }
-}
 
-@keyframes zoom-out {
-  from {
-    transform: scale(1, 1);
+  .fade-enter-active {
+    transition: all 0.25s linear;
   }
-  to {
-    transform: scale(0, 0);
+
+  .fade-leave-to {
+    transition: all 0.25s linear;
+    opacity: 0;
   }
-}
+
+  .zoom-enter-active {
+    animation: zoom-in 1s linear forwards;
+    transition: all 1s linear;
+  }
+
+  .zoom-leave-active {
+    animation: zoom-out 1s linear forwards;
+    transition: all 1s linear;
+  }
+
+  .zoom-enter-from {
+    opacity: 0;
+  }
+
+  .zoom-leave-to {
+    opacity: 0;
+  }
+
+  @keyframes zoom-in {
+    from {
+      transform: scale(0, 0);
+    }
+    to {
+      transform: scale(1, 1);
+    }
+  }
+
+  @keyframes zoom-out {
+    from {
+      transform: scale(1, 1);
+    }
+    to {
+      transform: scale(0, 0);
+    }
+  }
 </style>
