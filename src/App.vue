c195235a6c8cad5a9e9a0014d02bd3d665408684
Decio Neto <deciom.neto10@gmail.com>
2022-02-13 00:00:05 -0300

    feat: add css transitions

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,39 @@
 <template>
-  Animations
+  <button @click="changeFlag">Toggle</button>
+
+  <transition name="fade">
+    <h2 v-if="flag">Hello, World</h2>
+  </transition>
 </template>
 
 <script>
 
 export default {
   name: 'App',
+  methods: {
+    changeFlag() {
+      this.flag = !this.flag;
+    }
+  },
+  data() {
+    return {
+      flag: false,
+    }
+  }
 };
 </script>
+
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.25s linear;
+}
+
+.fade-leave-to {
+  transition: all 0.25s linear;
+  opacity: 0;
+}
+</style>
