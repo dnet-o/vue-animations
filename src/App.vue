b74d9b8f5ed7aa731365229b60df5dbea12ab2ac
Decio Neto <deciom.neto10@gmail.com>
2022-02-13 00:14:44 -0300

    feat: add keyframes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <button @click="changeFlag">Toggle</button>
 
-  <transition name="fade" mode="out-in">
+  <!-- <transition name="fade" mode="out-in">
     <h2 v-if="flag" key="main">Hello, World</h2>
     <h2 v-else key="secondary">Hello, Vue</h2>
+  </transition> -->
+
+  <transition name="zoom" type="animation" appear>
+    <h2 v-show="flag">Hello</h2>
   </transition>
 </template>
 
@@ -18,13 +22,18 @@ export default {
   },
   data() {
     return {
-      flag: false,
+      flag: true,
     }
   }
 };
 </script>
 
 <style>
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
 .fade-enter-from {
   opacity: 0;
 }
@@ -36,5 +45,41 @@ export default {
 .fade-leave-to {
   transition: all 0.25s linear;
   opacity: 0;
+}
+
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+  transition: all 1s linear;
+}
+
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+  transition: all 1s linear;
+}
+
+.zoom-enter-from {
+  opacity: 0;
+}
+
+.zoom-leave-to {
+  opacity: 0;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
+  }
+  to {
+    transform: scale(1, 1);
+  }
+}
+
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
+  }
+  to {
+    transform: scale(0, 0);
+  }
 }
 </style>
